82f243f2ff6bb9639ff813341e9ea838f879fbf5
Yoann FROMMELT <yfrommelt@hevaweb.com>
2016-11-28 10:02:32 +0100

    Création d'une class password avec des options

---
+++ Password.class.php
@@ -0,0 +1,87 @@
+<?php
+
+class Password
+{
+    private $length = 8;
+
+    private $charRanges = [
+        'upper' => 'ABCDEFGHJKLMNPQRSTUVWXYZ',
+        'lower' => 'abcdefghijkmnpqrstuvwxyz',
+        'digits' => '123456789',
+        'special' => '!@#$%^&*_+-=,./?;:`"~\'\\',
+        'brackets' => '(){}[]<>',
+        'high' => '¡¢£¤¥¦§©ª«¬®¯°±²³´µ¶¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ',
+        'ambiguous' => 'O0oIl'
+    ];
+
+    private $ranges = [];
+
+
+    public function __construct()
+    {
+
+    }
+
+    public function setLength($length)
+    {
+        $this->length = $length;
+    }
+
+
+    public function setOptionUpper($state)
+    {
+        $this->setOption('upper', $state);
+    }
+
+
+    public function setOptionLower($state)
+    {
+        $this->setOption('lower', $state);
+    }
+
+    public function setOptionDigits($state)
+    {
+        $this->setOption('digits', $state);
+    }
+
+    public function setOptionSpecial($state)
+    {
+        $this->setOption('special', $state);
+    }
+
+    public function setOptionBrackets($state)
+    {
+        $this->setOption('brackets', $state);
+    }
+
+    public function setOptionHigh($state)
+    {
+        $this->setOption('high', $state);
+    }
+
+    public function setOptionAmbiguous($state)
+    {
+        $this->setOption('ambiguous', $state);
+    }
+
+    public function setOption($option, $state)
+    {
+        if ($state) {
+            $this->ranges[$option] = $option;
+        } else {
+            unset($this->ranges[$option]);
+        }
+    }
+
+    public function generate()
+    {
+        $password = [];
+        for ($i = 0; $i < $this->length; $i++) {
+            $range = $this->ranges[array_rand($this->ranges)];
+            $char = mb_substr($this->charRanges[$range], rand(0, mb_strlen($this->charRanges[$range]) - 1), 1);
+            $password[] = $char;
+        }
+        return implode('', $password);
+    }
+
+}
--- ajax.php
+++ ajax.php
@@ -1,56 +1,69 @@
 <?php
+require_once 'Password.class.php';
+
+$response = array('succes' => false, 'message' => 'no action', 'passwords' => null);
 
 if (empty($_POST['action'])) {
-  $arr_json = array('succes'=>false, 'message'=>'pas d\'action', 'datas'=>null);
-	die(json_encode($arr_json));
+    die(json_encode($response));
 }
 
-if ($_POST['action'] == 'action_generer') {
-	$password = unique_random($_POST['length']);
-	die(sendHtml($password));
+switch ($_POST['action']) {
+    case 'generate':
+        $response['passwords'] = [generate($_POST)];
+        break;
+    case 'generate10':
+        $response['passwords'] = generate10($_POST);
+        break;
+    case 'encode':
+        $response['passwords'] = [encode($_POST['custom'])];
+        break;
 }
 
-if ($_POST['action'] == 'action_generer_10') {
-	$password_list = '';
-	for ($i = 0; $i < 10; $i++) {
-		$password = unique_random($_POST['length']);
-		$password_list .= getHtml($password);
-	}
-	$arr_json = array('succes'=>true, 'message'=>'crypté avec succès', 'datas'=>$password_list);
-	die(json_encode($arr_json));
+
+$response['succes'] = true;
+$response['message'] = $_POST['action'] . ' ok';
+
+die(json_encode($response));
+
+function generate($post)
+{
+    $password = getPasswordWithOptions($post);
+    return encode($password->generate());
 }
 
-if ($_POST['action'] == 'action_ajouter') {
-	$password = $_POST['custom'];
-	die(sendHtml($password));
-}
-
-function getHtml($password) {
-	$html_tr = '<tr><td class="clear">'.$password.'</td><td class="md5">'.md5($password).'</td><td class="crypt">'.crypt($password).'</td><td class="sha1">'.sha1($password).'</td></tr>';
-	return $html_tr;
-}
-
-function sendHtml($password) {
-	$html_tr = getHtml($password);
-	$arr_json = array('succes'=>true, 'message'=>'crypté avec succès', 'datas'=>$html_tr);
-	return json_encode($arr_json);
-}
-
-function unique_random($length = 8) {
-    $str_pass = '';
-    for ($i = 0; $i < $length; $i++) {
-        switch (mt_rand(1, 3)) {
-            case 1:
-                $char = mt_rand(65, 90);
-                break;
-            case 2:
-                $char = mt_rand(97, 122);
-                break;
-            case 3:
-                $char = mt_rand(48, 57);
-                break;
-        }
-        $str_pass .= chr($char);
+function generate10($post)
+{
+    $passwords = [];
+    for ($i = 0; $i < 10; $i++) {
+        $passwords[] = generate($post);
     }
-    return $str_pass;
+
+    return $passwords;
+}
+
+function encode($password)
+{
+    return [
+        'password' => $password,
+        'md5' => md5($password),
+        'crypt' => crypt($password, ''),
+        'sha1' => sha1($password),
+    ];
+}
+
+function getPasswordWithOptions($post)
+{
+    $password = new Password();
+
+    $password->setLength($post['length']);
+
+    $options = array_filter($post, function ($k) {
+        return preg_match('/^option_/', $k);
+    }, ARRAY_FILTER_USE_KEY);
+
+    foreach ($options as $option => $state) {
+        $password->setOption(explode('_', $option)[1], true);
+    }
+
+    return $password;
 }
